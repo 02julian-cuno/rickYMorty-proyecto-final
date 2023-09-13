b2994730e491e4624bb4b1f246b2a997b2c2f342
pedro frias <pedrotomasfrias1@gmail.com>
2023-09-13 16:57:14 -0300

    continuar-1era-parte

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,23 +4,25 @@
 function Home() {
     return(
         <header className="altura container-fluid d-flex flex-column justify-content-center align-items-center">
-            <h1 className="row justify-contant-center">Proyect Rick & Morty</h1>
+            <div className="fondo-blanco d-flex flex-column justify-content-center">
+                 <h1 className="row justify-content-center">Proyect Rick & Morty</h1>
 
-            <h2 className="text-center p-3">Helcome to Rick & Morty Proyect!</h2>
+                 <h2 className="text-center p-3">Helcome to Rick & Morty Proyect!</h2>
 
-            <p className="text-center p-2">This proyect was made for practising React and to made a functional website</p>
+                 <p className="text-center p-2">This proyect was made for practising React and to made a functional website</p>
 
-            <p className="text-center">In this website you can know information of the characters of this animated series.
+                <p className="text-center">In this website you can know information of the characters of this animated series.
             
-            <br/>
+                    <br/>
 
-            Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion</p>
+                    Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion</p>
 
             <h2 className="text-center p-3">Lets go!</h2>
 
-            
-
             <Navegacion/>
+
+            </div>
+           
         </header>
     )
         
