7b5e338e0bc4f27a217e3a336f4becbe8eb9bae5
pedro frias <pedrotomasfrias1@gmail.com>
2023-09-13 14:54:17 -0300

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,29 @@
+// import Navegacion from "";
+
+function Home() {
+    return(
+        <header>
+            <h1>Proyect Rick & Morty</h1>
+
+            <h2>Helcome to Rick & Morty Proyect!</h2>
+
+            <p>This proyect was made for practising React and to made a functional website</p>
+
+            <p>In this website you can know information of the characters of this animated series.</p>
+
+            <p>Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion</p>
+
+            <h2>Lets go!</h2>
+
+            <nav>
+
+            </nav>
+
+
+
+        </header>
+    )
+        
+    
+}
+export default Home;
