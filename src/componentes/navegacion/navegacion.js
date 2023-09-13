d4f0eff4bc25f378fa2adae733143162ca33918e
pedro frias <pedrotomasfrias1@gmail.com>
2023-09-13 16:25:57 -0300

    primera-parte

---
+++ src/componentes/navegacion/navegacion.js
@@ -0,0 +1,20 @@
+
+import  "./navegacion.css";
+
+
+
+function Navegacion({}) {
+    return(
+        <nav className="row navbar align-items-center">
+
+          <ul className="nav nav-pills justify-content-center">
+
+            <li className="nav-item"><a className="nav-link active m-2 p-3 boton ">Characters</a></li>
+            <li className="nav-item"><a className="nav-link active m-2 p-3 boton">Contact</a></li>
+            
+          </ul>
+          
+        </nav> 
+    )
+}
+export default Navegacion;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,26 @@
-// import Navegacion from "";
+ import Navegacion from "../componentes/navegacion/navegacion";
+ import "../home.css"
 
 function Home() {
     return(
-        <header>
-            <h1>Proyect Rick & Morty</h1>
+        <header className="altura container-fluid d-flex flex-column justify-content-center align-items-center">
+            <h1 className="row justify-contant-center">Proyect Rick & Morty</h1>
 
-            <h2>Helcome to Rick & Morty Proyect!</h2>
+            <h2 className="text-center p-3">Helcome to Rick & Morty Proyect!</h2>
 
-            <p>This proyect was made for practising React and to made a functional website</p>
+            <p className="text-center p-2">This proyect was made for practising React and to made a functional website</p>
 
-            <p>In this website you can know information of the characters of this animated series.</p>
+            <p className="text-center">In this website you can know information of the characters of this animated series.
+            
+            <br/>
 
-            <p>Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion</p>
+            Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion</p>
 
-            <h2>Lets go!</h2>
+            <h2 className="text-center p-3">Lets go!</h2>
 
-            <nav>
+            
 
-            </nav>
-
-
-
+            <Navegacion/>
         </header>
     )
         
